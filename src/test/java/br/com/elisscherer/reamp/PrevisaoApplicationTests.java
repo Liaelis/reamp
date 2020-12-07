04f880199ef9061916f7ea9242221a371ddfb729
Liaelis <er.paisagista@gmail.com>
2020-12-07 08:27:10 -0300

    criação projeto
    projeto java + springboot

---
+++ src/test/java/br/com/elisscherer/reamp/PrevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elisscherer.reamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
